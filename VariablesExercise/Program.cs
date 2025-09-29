bd9fced59c1e1bb9a8978f159474e5d58104c773
Arnesha Jackson <ArneshaJackson1@gmail.com>
2025-09-29 10:29:24 -0400

    You're welcome

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string catName = "Cali";
+            int age = 10;
+            char iniital = 'B';
+            bool married = true;
+            double houses = 40.00;
+            decimal islands = 10.3m;
+
+            Console.WriteLine($"My cat's name is {catName}, She is {age} years old");
+            Console.WriteLine($"Married: {married} and has {houses} houses. On {islands} islands");
         }
     }
 }
